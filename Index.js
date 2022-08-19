2c2e8f852a8f62cb5c439238d5bd37c5fad95fd0
DeRep311 <77757535+DeRep311@users.noreply.github.com>
2022-08-19 00:32:28 +0000

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,16 @@
+
+//Requerimientos
+const express = require('express');
+const app =express();
+const {router}= require('./src/Routes/indexRoutes.js')
+
+
+const PORT= 8080;
+app.listen(PORT,()=>{
+    console.log("El server escucha");
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use('/api',require('./src/Routes/indexRoutes.js'));
+
+
---
+++ src/Routes/API.js
@@ -0,0 +1,109 @@
+const router=require('express').Router();
+
+let productos=[
+    {
+        "title": "(nombre del producto)",
+        "price": "(precio)aaa",
+        "thumbnail": "(url al logo o foto del producto)",
+        "id":1
+    },
+    {
+        "title": "(nombre del producto)",
+        "price": "(precioeee)",
+        "thumbnail": "(url al logo o foto del producto)",
+        "id":2
+    },
+    {
+        "title": "(nombre del producto)",
+        "price": "(precio000)",
+        "thumbnail": "(url al logo o foto del producto)",
+        "id":3
+    }
+    
+]
+
+
+
+
+
+router.get('/',(req,res)=>{
+    res.json(productos)
+    
+    
+    
+    
+    
+})
+router.get('/:id',(req,res)=>{
+    const { id }=req.params;
+    const ProduID=productos.filter(productos=>productos.id==id);
+    if (ProduID.length===0) {
+        res.send("Error Producto no existe").status(400)
+    }else{
+        res.send(ProduID)
+    }
+    
+
+})
+router.post('/',(req,res)=>{
+    let nombre=req.body.title;
+    let price=req.body.price;
+    let thumbnail=req.body.thumbnail;
+    let idnew=(productos.length)+1;
+
+    const ingreso= {
+        "title": nombre,
+        "price": price,
+        "thumbnail": thumbnail,
+        "id":idnew
+    }
+    productos.push(ingreso);
+    console.log("ingresado");
+    res.json(productos)
+    
+})
+router.put('/:id',(req,res)=>{
+    
+    const {id}=req.params;
+    const ProduID=productos.findIndex(productos=>{
+        return productos.id==id;
+    });
+
+    
+    if (ProduID==-1) {
+        res.send("Error Producto no existe").status(400)
+    }else{
+        productos[ProduID]=req.body;
+    
+    
+        res.json(productos);
+    
+
+    // const ingreso= {
+    //     "title": req.body.nombre,
+    //     "price": req.body.price,
+    //     "thumbnail": req.body.thumbnail,
+    //  }
+    //productos[ProduID]= ingreso;
+    // 
+}
+    
+   
+    
+    
+})
+router.delete('/:id',(req,res)=>{
+    const { id }=req.params;
+    const ProduID=productos.filter(productos=>productos.id!=id);
+    if (ProduID.length===0) {
+        res.send("no existe").status(400)
+    }else{
+        productos=ProduID;
+        
+        console.log("eliminado");
+        
+      res.json(productos);
+        
+    }
+})
+module.exports=router
